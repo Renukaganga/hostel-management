9831896f97c0aa6b9dd2b6f301a1dcf2412b6714
Renukaganga <22b01a1231@svecw.edu.in>
2024-07-25 20:46:34 +0530

    first-commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div 
+      className="h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat bg-fixed" 
+      style={{ backgroundImage: 'url("https://5.imimg.com/data5/MV/WT/YQ/SELLER-53514865/hostel-management-software-500x500.png")' }}
+    >
+      <div className="bg-white bg-opacity-40 text-black p-8 rounded-lg text-center shadow-lg">
+        {/* <h1 className="text-4xl font-bold mb-4">Welcome to <span className='text-blue-600'>Hostel Complaint Management</span></h1>
+        <p className="text-lg mb-6">Manage all your hostel complaints efficiently and effectively.</p> */}
+        <div className='mt-5'>
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
